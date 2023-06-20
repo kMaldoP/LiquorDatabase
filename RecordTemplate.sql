70572e168a3fefd6860d540b9448186d54cc03fa
krysmaldonado <krysm2015@gmail.com>
2023-06-19 20:28:45 -0700

    added additional tables

--- RecordTemplate.sql
+++ RecordTemplate.sql
@@ -8,3 +8,53 @@ CREATE TABLE Inventory(
     CostPerPour MONEY NOT NULL,
     StandardRetail MONEY NOT NULL
 );
+
+CREATE TABLE Rum AS
+   SELECT ProductName,BottleSize,BottleCost,CostPerOunce,CostPerPour,StandardRetail
+   FROM inventory
+   WHERE category = 'RUM'
+
+CREATE TABLE Vodka AS
+   SELECT ProductName,BottleSize,BottleCost,CostPerOunce,CostPerPour,StandardRetail
+   FROM inventory
+   WHERE category = 'VODKA'
+
+CREATE TABLE Gin AS
+   SELECT ProductName,BottleSize,BottleCost,CostPerOunce,CostPerPour,StandardRetail
+   FROM inventory
+   WHERE category = 'GIN'
+
+CREATE TABLE TEQUILA AS
+   SELECT ProductName,BottleSize,BottleCost,CostPerOunce,CostPerPour,StandardRetail
+   FROM inventory
+   WHERE category = 'TEQUILA'
+
+CREATE TABLE MEZCAL AS
+   SELECT ProductName,BottleSize,BottleCost,CostPerOunce,CostPerPour,StandardRetail
+   FROM inventory
+   WHERE category = 'MEZCAL'
+
+CREATE TABLE LiquerCordial AS
+   SELECT ProductName,BottleSize,BottleCost,CostPerOunce,CostPerPour,StandardRetail
+   FROM inventory
+   WHERE category = 'LIQUER/CORDIAL'
+
+CREATE TABLE WHISKEY AS
+   SELECT ProductName, category,BottleSize,BottleCost,CostPerOunce,CostPerPour,StandardRetail
+   FROM inventory
+   WHERE category = 'WHISKEY/BOURBON'OR 
+   category = 'WHISKEY/OTHER' OR 
+   category = 'WHISKEY/IRISH' OR 
+   category = 'WHISKEY/SCOTCH' OR 
+   category = 'WHISKEY/RYE' OR
+   category = 'WHISKEY'
+
+CREATE TABLE vermouthAmaro AS
+   SELECT ProductName,BottleSize,BottleCost,CostPerOunce,CostPerPour,StandardRetail
+   FROM inventory
+   WHERE category = 'VERMOUTH/AMARO'
+
+   CREATE TABLE brandyCognac AS
+   SELECT ProductName,BottleSize,BottleCost,CostPerOunce,CostPerPour,StandardRetail
+   FROM inventory
+   WHERE category = 'BRANDY/CONGAC'
